c454b44bc216e3ca734a039a777b5aaa87738c05
calvinguyen <cnguyenr42@gmail.com>
2022-04-06 16:25:23 -0500

    added GET apis for tables

--- backend/crudAPIs.py
+++ backend/crudAPIs.py
@@ -11,7 +11,7 @@ app.config["DEBUG"] = True #allow to show errors in browser
 def home():
     return "<h1>Test Connection</h1>"
 
-# returns table data
+#(DONE) returns Customers table
 @app.route('/api/customers', methods=['GET'])
 def getcustomers():
     #connect to database 
@@ -21,6 +21,58 @@ def getcustomers():
     customers = execute_read_query(cnxn, query)
     return jsonify(customers)
 
+#(DONE) returns Companies tables
+@app.route('/api/companies', methods=['GET'])
+def getcompanies():
+    #connect to database 
+    cnxn = create_cnxn('172.26.54.133', 'Synfoco_Logistic_Management_System', 'synfoco', 'Synfoco16')
+    query = 'SELECT * FROM Companies'
+    #execute sql return statement and get customer records
+    companies = execute_read_query(cnxn, query)
+    return jsonify(companies)
+
+#(DONE) returns Drivers tables
+@app.route('/api/drivers', methods=['GET'])
+def getdrivers():
+    #connect to database 
+    cnxn = create_cnxn('172.26.54.133', 'Synfoco_Logistic_Management_System', 'synfoco', 'Synfoco16')
+    query = 'SELECT * FROM Drivers'
+    #execute sql return statement and get customer records
+    drivers = execute_read_query(cnxn, query)
+    return jsonify(drivers)
+
+#DONE returns Employees tables
+@app.route('/api/employees', methods=['GET'])
+def getemployees():
+    #connect to database 
+    cnxn = create_cnxn('172.26.54.133', 'Synfoco_Logistic_Management_System', 'synfoco', 'Synfoco16')
+    query = 'SELECT * FROM Employees'
+    #execute sql return statement and get customer records
+    employees = execute_read_query(cnxn, query)
+    return jsonify(employees)
+
+#DONE returns Employees tables
+@app.route('/api/orders', methods=['GET'])
+def getorders():
+    #connect to database 
+    cnxn = create_cnxn('172.26.54.133', 'Synfoco_Logistic_Management_System', 'synfoco', 'Synfoco16')
+    query = 'SELECT * FROM Orders'
+    #execute sql return statement and get customer records
+    orders = execute_read_query(cnxn, query)
+    return jsonify(orders)
+
+#DONE Delete record in  tables
+@app.route('/api/orders', methods=['GET'])
+def getorders():
+    #connect to database 
+    cnxn = create_cnxn('172.26.54.133', 'Synfoco_Logistic_Management_System', 'synfoco', 'Synfoco16')
+    query = 'SELECT * FROM Orders'
+    #execute sql return statement and get customer records
+    orders = execute_read_query(cnxn, query)
+    return jsonify(orders)
+
+
+
 @app.route('/api/testpost', methods=['POST'])
 def testpost():
     req_data = request.get_json()
@@ -28,18 +80,7 @@ def testpost():
     newfn = req_data['firstname']
     return 'ID: {}, Name: {}'.format(newid, newfn)
 
-# test connection
-@app.route('/api/cnxn', methods=['GET'])
-def testcnxn():
-    #connect to database 
-    cnxn = create_cnxn('172.26.54.133', 'Synfoco_Logistic_Management_System', 'synfoco', 'Synfoco16')
-    query = """
-    SELECT * FROM Customers
-    WHERE ID = 1
-    """
-    #execute sql return statement and get customer records
-    customers = execute_read_query(cnxn, query)
-    return jsonify(customers)
+
 
 
 
